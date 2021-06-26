8e3ab7c51028145ba67444f88d1351f6e0ed3dcf
Ardaerd <arderd@outlook.com>
2021-06-26 12:56:33 +0300

    addInOrder method is added

--- src/Section_8_Arrays_InbuiltList_Autoboxing_Unboxing/LinkedList_Lesson/main.java
+++ src/Section_8_Arrays_InbuiltList_Autoboxing_Unboxing/LinkedList_Lesson/main.java
@@ -2,6 +2,7 @@ package Section_8_Arrays_InbuiltList_Autoboxing_Unboxing.LinkedList_Lesson;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class main {
     public static void main(String[] args) {
@@ -30,5 +31,28 @@ public class main {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("=======================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination.");
+                return false;
+            } else if (comparison > 0) {
+                // new city should appear before this one
+                // Brisbane -> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // move on the next city
+            }
+        }
+        stringListIterator.add(newCity);
     }
 }
