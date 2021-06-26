eb9168be62399ca2b0da4b112fa9ea12732318e2
Ardaerd <arderd@outlook.com>
2021-06-26 12:24:12 +0300

    adding and removing example

--- src/Section_8_Arrays_InbuiltList_Autoboxing_Unboxing/LinkedList_Lesson/main.java
+++ src/Section_8_Arrays_InbuiltList_Autoboxing_Unboxing/LinkedList_Lesson/main.java
@@ -1,7 +1,34 @@
 package Section_8_Arrays_InbuiltList_Autoboxing_Unboxing.LinkedList_Lesson;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class main {
     public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
 
+        printList(placesToVisit);
+
+        placesToVisit.add(1,"Alice Springs");
+        printList(placesToVisit);
+
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+
+        while (i.hasNext()) {
+            System.out.println("Now visiting " + i.next());
+        }
+        System.out.println("=======================");
     }
 }
